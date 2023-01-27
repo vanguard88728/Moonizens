35dc6fd10a2d7954f51871fc4641ead41b0dbc1b
Alex <vanguard88728@gmail.com>
2023-01-27 08:33:48 +0800

    Add animation to card

--- src/components/SectionCard/SectionCard.js
+++ src/components/SectionCard/SectionCard.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import './SectionCard.css';
 
 const SectionCard = ({ cardClassName, children }) => {
 
   return (
-    <div className={`section-card ${cardClassName}`}>
+    <motion.div
+      className={`section-card ${cardClassName}`}
+      initial={{ opacity: 0, scale: 1.25 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      viewport={{ once: true }}
+      transition={{
+        default: {
+          duration: 1,
+          ease: [0.76, 0, 0.24, 1]
+        }
+      }}
+    >
       <div className="section-card-border"></div>
       { children }
-    </div>
+    </motion.div>
   );
 };
 
